2dfc1533c3f006ae0a66439d392ea85a56eb8c71
Sophie Creutz <sophie.creutz@flatironschool.com>
2019-08-18 17:26:42 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,63 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+  
 
+  def self.find_by_name(name)
+    self.all.find{|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+      find_by_name(name) || create_by_name(name)
+  end
+  
+  def self.alphabetical
+    @@all.sort_by{|song| song.name}
+  end
+  
+  def self.new_from_filename(filename)
+    array = filename.split(" - ")
+    song = self.new 
+    name = array[1]
+    name2 = name[0...-4]
+    song.name = name2
+    song.artist_name = array[0]
+    song
+  end
+  
+  def self.create_from_filename(filename)
+    array = filename.split(" - ")
+    song = self.new 
+    name = array[1]
+    name2 = name[0...-4]
+    song.name = name2
+    song.artist_name = array[0]
+    @@all << song
+    song
+  end
+    
+  def self.destroy_all
+    @@all.clear
+  end
+
+  
 end
